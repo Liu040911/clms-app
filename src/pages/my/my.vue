d78ca10c6bfe7d14dd56c6f3f0554239c2f7143e
百炼成才 <1606294640@qq.com>
2026-05-08 17:39:05 +0800

    app: replace legacy qiniu CDN domain with custom domain

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -143,7 +143,7 @@ function uploadImageByFilePath(filePath: string, token: string, key: string): Pr
             const uploadData = typeof uploadRes.data === 'string'
               ? JSON.parse(uploadRes.data || '{}')
               : (uploadRes.data as Record<string, any>)
-            resolve(`http://td4d4v1ov.hn-bkt.clouddn.com/${uploadData?.key || key}`)
+            resolve(`http://file.liuservice.online/${uploadData?.key || key}`)
             return
           }
           tryUpload()
